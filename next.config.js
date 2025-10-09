26b358fc3e21f5600f21e2731cf78ff96314b43f
ahwurm <ahwurm1@gmail.com>
2025-10-09 14:03:14 -0400

    fixed config for github pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
+  basePath: isProd ? '/awurm-web' : '',
+  assetPrefix: isProd ? '/awurm-web/' : '',
 }
 
 module.exports = nextConfig
